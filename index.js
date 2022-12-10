c66f947ef4c31c17747fc533a722998f911bfe37
David <david.owuor@student.moringaschool.com>
2022-12-10 11:02:07 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i=0; i<array.length; i++){
+    for(let j= i+1; j<array.length; j++){
+      if(array[i]+array[j]===target){
+        return true
+      }
+    }
+  }
+  return false
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
 */
+// n(0)
 
 /* 
   Add your pseudocode here
-*/
+// *///create an array of Numbers,
+// iterate through them using index and sum up two numbers
+// if sum is equal to target, return true
+// if its not, return false
 
 /*
   Add written explanation of your solution here
 */
+// list array according to index value
+// declare variable i for first index element
+// declare variable j for second index element
+// iterate through the whole array, adding i and j
+// compare is sum of i, j = target
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
